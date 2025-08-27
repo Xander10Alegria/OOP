e3de1ad698d6d0dfbb8bf11936f3bfc21bc70eb1
Xander10Alegria <Xander10Alegria@gmail.com>
2025-08-27 09:52:21 -0500

    Add files via upload

---
+++ cs xander1.py
@@ -0,0 +1,11 @@
+stu_name = input("student:")
+
+course1 = int (input("enter grade course1:") )
+course2 = int (input("enter grade course2:") )
+course3 = int (input("enter grade course3:") )
+course4 = int (input("enter grade course4:") )
+course5 = int (input("enter grade course5:") )
+
+total = course1+course2+course3+course4+course5
+print(total)
+print(total/5)
